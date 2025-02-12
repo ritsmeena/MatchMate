f26f543c9ec7dc6f7e391ffbdf05acfeebefc279
Ritika Meena <ritikameena17dec@@gmail.com>
2025-02-13 00:25:02 +0530

    Final Card Design

--- MatchMate/MatchMate/Views/MateCardView.swift
+++ MatchMate/MatchMate/Views/MateCardView.swift
@@ -14,25 +14,26 @@ struct MateCardView: View {
     var onAction: () -> Void
     
     @State private var offset = CGSize.zero
-
+    
     var body: some View {
+        
         ZStack {
             if let imageUrl = URL(string: profile.picture.large) {
                 AsyncImage(url: imageUrl) { image in
                     image.resizable()
                         .scaledToFill()
-                        .frame(width: 300, height: 400)
+                        .frame(width: 350, height: 600)
                         .clipped()
                         .cornerRadius(20)
                         .shadow(radius: 5)
                 } placeholder: {
                     Color.gray
-                        .frame(width: 300, height: 400)
+                        .frame(width: 350, height: 600)
                         .cornerRadius(20)
                 }
             }
-
-            VStack {
+            
+            VStack(spacing: 0){
                 Spacer()
                 HStack {
                     Text("\(profile.name.first), \(profile.dob.age)")
@@ -42,37 +43,51 @@ struct MateCardView: View {
                         .shadow(radius: 2)
                     Spacer()
                 }
-                .padding()
+                .padding(.leading)
+                .background(Color.black.opacity(0.5))
+                
+                HStack{
+                    Text("\(profile.location.street.number) - \(profile.location.street.name), \(profile.location.city ), \(profile.location.state ), \(profile.location.country )")
+                        .font(.subheadline)
+                        .foregroundColor(.white)
+                        .shadow(radius: 2)
+                    Spacer()
+                }
+                .padding(.leading)
+                .background(Color.black.opacity(0.5))
+                
+                HStack {
+                    Button(action: {
+                        print("\(profile.name.first) disliked")
+                        onAction()
+                        resetOffset()
+                    }) {
+                        Image(systemName: "xmark.circle")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .foregroundColor(.red)
+                            .padding()
+                    }
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        print("\(profile.name.first) liked")
+                        onAction()
+                        resetOffset()
+                    }) {
+                        Image(systemName: "heart.circle")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .foregroundColor(.green)
+                            .padding()
+                    }
+                }
                 .background(Color.black.opacity(0.5))
             }
             .cornerRadius(20)
-            .frame(width: 300, height: 400)
-
-            HStack {
-                Button(action: {
-                    print("\(profile.name.first) disliked")
-                    onAction()
-                }) {
-                    Image(systemName: "xmark.circle")
-                        .resizable()
-                        .frame(width: 50, height: 50)
-                        .foregroundColor(.red)
-                        .padding()
-                }
-                Spacer()
-                Button(action: {
-                    print("\(profile.name.first) liked")
-                    onAction()
-                }) {
-                    Image(systemName: "heart.circle")
-                        .resizable()
-                        .frame(width: 50, height: 50)
-                        .foregroundColor(.green)
-                        .padding()
-                }
-            }
-            .frame(width: 300)
-            .position(x: UIScreen.main.bounds.width / 2, y: 500)
+            .frame(width: 350,height: 600)
+            
         }
         .offset(x: offset.width, y: offset.height)
         .rotationEffect(.degrees(Double(offset.width / 20)))
@@ -89,8 +104,12 @@ struct MateCardView: View {
                         print("\(profile.name.first) disliked")
                         onAction()
                     }
-                    offset = .zero
+                    resetOffset()
                 }
         )
+    }
+    
+    private func resetOffset() {
+        offset = .zero
     }
 }
